ad20f8ae8a056d9ea136c5f38e7fcb44155faedc
Wolfchamane <arturo.mart.diaz@gmail.com>
2021-02-18 10:37:54 +0100

    [chore] (*): updated deprecated dependencies

--- config/vue.js
+++ config/vue.js
@@ -3,7 +3,7 @@
  * propiedades de configuración.
  */
 import Vue from 'vue';
-import VuePropSep from 'vue-prop-sep';
+import VuePropSep from '@jf/vue-prop-sep';
 
 // ------------------------------------------------------------------------------
 // Configuración de vue.
